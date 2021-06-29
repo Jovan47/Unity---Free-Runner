d49e1150d94a89b404d05143875db2bd09e62e7b
Jovan <radulovic.jovan.ni@gmail.com>
2021-06-29 23:24:24 +0200

    Novi igrac, kretanje

--- New Unity Project/Assets/PlayerMovement.cs
+++ New Unity Project/Assets/PlayerMovement.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector3 maxPlace;
+    Vector3 minPlace;
+    Vector3 nextPlace;
+    bool moved = false;
+   
     void Start()
     {
+        maxPlace = new Vector3(16, 0, 16);
+        minPlace = new Vector3 (0, 0, 0);
+        Vector3 nextPlace = new Vector3(0, 0, 0);
         Application.targetFrameRate = 70;
-
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+
+        if      (Input.GetKeyDown(KeyCode.UpArrow)   || Input.GetKeyDown("w")) { nextPlace = transform.position + new Vector3(4,0,0);  moved = true; }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown("a")) { nextPlace = transform.position + new Vector3(0,0,4);  moved = true; }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown("s")) { nextPlace = transform.position + new Vector3(-4,0,0); moved = true; }
+        else if (Input.GetKeyDown(KeyCode.RightArrow)|| Input.GetKeyDown("d")) { nextPlace = transform.position + new Vector3(0,0,-4); moved = true; }
+
+        if (nextPlace.x<=36 &&nextPlace.x>=0 &&nextPlace.z<=36 &&nextPlace.z>=0 && moved)
+        {
+            transform.position = nextPlace;
+            moved = false;
+        }
+
+        Debug.Log(transform.position);
     }
 }
