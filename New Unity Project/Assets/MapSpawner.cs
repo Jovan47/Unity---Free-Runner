2e1556f6a6f5a0a2d71d89cc8f8ce38ab3aa68af
Jovan <radulovic.jovan.ni@gmail.com>
2021-07-02 02:00:17 +0200

    Endless NADOLE

--- New Unity Project/Assets/MapSpawner.cs
+++ New Unity Project/Assets/MapSpawner.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MapSpawner : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> terrains = new List<GameObject> ();
     [SerializeField] private float playerJumpDistance = 4;
     public GameObject TerrainPrefab;
     public Color      secondColor;
@@ -11,6 +12,14 @@ public class MapSpawner : MonoBehaviour
     private List<GameObject> listaTile;
     private GameObject       [,]mapa;
     private int              indexListe;
+
+    private bool left   = false;
+    private bool right  = false;
+    private bool down   = false;
+    private bool up     = false;
+    private bool moved = false;
+    private int numberOfTiles = 10;
+
     void Start()
     {
 
@@ -18,6 +27,7 @@ public class MapSpawner : MonoBehaviour
         Vector3 position = new Vector3(0, 0, 0);
         mapa = new GameObject[10, 10];
         indexListe = 0;
+
 
         for (int i=0; i<10; i++)
         {
@@ -30,12 +40,56 @@ public class MapSpawner : MonoBehaviour
                 GameObject tempObj = mapa[i, j];
                 tempObj.transform.parent = gameObject.transform;
                 listaTile.Add(tempObj);
-                if (i == j || (i+j)==10-1)
+                /* 
+                 if (i == j || (i+j)==10-1)
+                 {
+                     Renderer rend = tempObj.GetComponent<Renderer>();
+                     rend.material.color = secondColor;
+                 }
+                */
+                Renderer rend = tempObj.GetComponent<Renderer>();
+
+                if (i == 0)
                 {
-                    Renderer rend = tempObj.GetComponent<Renderer>();
-                    rend.material.color = secondColor;
-                    
+                    rend.material.color = Color.cyan;
                 }
+                if (i == 1)
+                {
+                    rend.material.color = Color.white;
+                }
+                if (i == 2)
+                {
+                    rend.material.color = Color.black;
+                }
+                if (i == 3)
+                {
+                    rend.material.color = Color.gray;
+                }
+                if (i == 4)
+                {
+                    rend.material.color = Color.green;
+                }
+                if (i == 5)
+                {
+                    rend.material.color = Color.blue;
+                }
+                if (i == 6)
+                {
+                    rend.material.color = Color.red;
+                }
+                if (i == 7)
+                {
+                    rend.material.color = Color.yellow;
+                }
+                if (i == 8)
+                {
+                    rend.material.color = new Color32(173, 173, 21, 200);
+                }
+                if (i == 9)
+                {
+                    rend.material.color = new Color32(100, 2, 21, 12);
+                }
+
                 tempObj.SetActive(false);
                 // LeanTween.scale(tempObj, new Vector3(3f, 0.1f, 3f), 2f).setEase(LeanTweenType.easeInSine);
             }
@@ -50,7 +104,10 @@ public class MapSpawner : MonoBehaviour
     IEnumerator TweenIng()
     {
         yield return new WaitForSeconds(3);
-        if (indexListe == listaTile.Count) { yield break; }
+        if (indexListe == listaTile.Count) 
+        { 
+            yield break; 
+        }
         GameObject temp = listaTile[indexListe];
         indexListe += 1;
         temp.SetActive(true);
@@ -58,9 +115,120 @@ public class MapSpawner : MonoBehaviour
     }
 
 
+    
+
     void Update()
     {
+        if (moved)
+        {   
+            if(right)
+            {
+                MoveTilesRight();
+            }
+             if (left)
+            {
+                MoveTilesLeft();
+            }
+             if (up)
+            {
+                MoveTilesUp();
+            }
+            if (down)
+            {
+                MoveTilesDown();
+                down = false;
+                Debug.Log("POZAVNA DA IDE DOLE!!");
+            }
+            moved = false;
+
+        }
+
+    }
+
+
+
+    public void MoveTiles(string s)
+    {
+        if (s == "left")
+        {
+            this.left = true;
+        }
+        else if (s == "right")
+        {
+            this.right = true;
+        }
+        else if (s == "down")
+        {
+           // Debug.Log("POMERI MAPU DOLE");
+
+            down = true;
+        }
+        else if (s == "up")
+        {
+            up = true;
+        }
+        moved = true;
+    }
+    
+
+    public void MoveTilesDown()
+    {   
+        for(int i=0; i< numberOfTiles; i++)
+        {
+            for(int j=0; j< numberOfTiles; j++)
+            {
+                if (i ==numberOfTiles-1)
+                {
+                    mapa[i, j].transform.position = mapa[0, j].transform.position + new Vector3(-4, 0,0);
+
+                }
+            }
+        }
+        SwapCoulumnsUP();
 
 
     }
+    public void MoveTilesUp()
+    {
+
+    }
+    public void MoveTilesLeft()
+    {
+
+    }
+    public void MoveTilesRight()
+    {
+
+    }
+
+    public void SwapCoulumnsUP()
+    {
+        GameObject[] arrayTmp = new GameObject[numberOfTiles];
+       
+        for(int i=0; i<10; i++)
+        {
+            arrayTmp[i] = mapa[numberOfTiles - 1, i];
+            Renderer ob= arrayTmp[i].GetComponent<Renderer>();
+            ob.material.color = Color.red;
+
+        }
+
+        for(int i=numberOfTiles-1; i>0; i--)
+        {
+            for (int j = numberOfTiles - 1; j >= 0; j--)
+            {
+                mapa[i, j] = mapa[i - 1, j];
+               
+            }
+        }
+        
+        
+        
+        for(int i=0; i<10; i++)
+        {
+            mapa[0, i] = arrayTmp[i];
+        }
+
+    }
+
 }
--- New Unity Project/Assets/PlayerMovement.cs
+++ New Unity Project/Assets/PlayerMovement.cs
@@ -6,17 +6,22 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float maxX;
     [SerializeField] private float maxZ;
-    [SerializeField] private float offSetPlayerMove=4;
-    [SerializeField] private float rayReach=4;
-                     private bool moved;
-                     private bool isHoping;
-                     private Vector3 nextPlace;
-                     private Vector3 currentDiretcion;
-                     private Animator animator;
-                     
+    [SerializeField] private float offSetPlayerMove = 4;
+    [SerializeField] private float rayReach = 4;
+    private bool moved;
+    private bool isHoping;
+    private Vector3 nextPlace;
+    private Vector3 currentDiretcion;
+    private Animator animator;
+    public GameObject MapSpawner = null;
+    private MapSpawner scriptRef = null;
+
+    private string directionMove;
+
     void Start()
     {
-        currentDiretcion= new Vector3(0, 0, 0);
+        scriptRef = MapSpawner.GetComponent<MapSpawner>();
+        currentDiretcion = new Vector3(0, 0, 0);
         moved = false;
         Vector3 nextPlace = new Vector3(0, 0, 0);
         Application.targetFrameRate = 70;
@@ -24,57 +29,68 @@ public class PlayerMovement : MonoBehaviour
     }
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow)   || Input.GetKeyDown("w") && !isHoping)
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown("w") && !isHoping)
         {
-            nextPlace = transform.position + new Vector3(offSetPlayerMove, 0,0); 
+            nextPlace = transform.position + new Vector3(offSetPlayerMove, 0, 0);
             currentDiretcion = (nextPlace - transform.position).normalized;
             moved = true;
+            directionMove = "up";
         }
 
-        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown("a") && !isHoping) 
-        { 
-            nextPlace = transform.position + new Vector3(0,0,offSetPlayerMove);  
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown("a") && !isHoping)
+        {
+            nextPlace = transform.position + new Vector3(0, 0, offSetPlayerMove);
             currentDiretcion = (nextPlace - transform.position).normalized;
             moved = true;
+            directionMove = "left";
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown("s") && !isHoping)
-        { 
-            nextPlace = transform.position + new Vector3(-offSetPlayerMove, 0,0);
+        {
+            nextPlace = transform.position + new Vector3(-offSetPlayerMove, 0, 0);
             currentDiretcion = (nextPlace - transform.position).normalized;
             moved = true;
+            directionMove = "down";
+
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow)|| Input.GetKeyDown("d") && !isHoping)
-        {   
-            nextPlace = transform.position + new Vector3(0,0,-offSetPlayerMove); 
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown("d") && !isHoping)
+        {
+            nextPlace = transform.position + new Vector3(0, 0, -offSetPlayerMove);
             currentDiretcion = (nextPlace - transform.position).normalized;
             moved = true;
+            directionMove = "right";
+
 
         }
 
-        if (nextPlace.x<= maxX && nextPlace.x>=0 &&nextPlace.z<= maxZ && nextPlace.z>=0 && moved && !isHoping)
+        if (nextPlace.x <= maxX && nextPlace.x >= 0 && nextPlace.z <= maxZ && nextPlace.z >= 0 && moved && !isHoping)
         {
             LeanTween.move(gameObject, nextPlace, 0.28f);
             animator.SetTrigger("hop");
             isHoping = true;
-            moved = false;  
+            moved = false;
+
+            scriptRef.MoveTiles(directionMove);
         }
-        
+
         RaycastHit hit;
-        Debug.DrawRay(nextPlace + 2*currentDiretcion, currentDiretcion * rayReach, Color.red);
+        Debug.DrawRay(nextPlace + 2 * currentDiretcion, currentDiretcion * rayReach, Color.red);
         if (Physics.Raycast(nextPlace + 2 * currentDiretcion, currentDiretcion * rayReach, out hit))
         {
             if (hit.transform.tag == "Tile")
             {
-                Debug.Log("Udarili smo u " + hit.transform.tag + " at position " +hit.transform.position);
+                //Debug.Log("Udarili smo u " + hit.transform.tag + " at position " +hit.transform.position);
             }
         }
-        else { Debug.Log("Nema dalje"); }
-       
-    }
+        else
+        { //Debug.Log("Nema dalje"); }
 
+        }
 
-    public void FinishedHop()
-    {
-        isHoping = false;
     }
+        public void FinishedHop()
+        {
+            isHoping = false;
+        }
+     
+
 }
