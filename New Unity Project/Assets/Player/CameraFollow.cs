8efa3010f167527aebee0f6ff72771644d4d0111
Jovan <radulovic.jovan.ni@gmail.com>
2021-07-07 23:38:50 +0200

    no message

--- New Unity Project/Assets/Player/CameraFollow.cs
+++ New Unity Project/Assets/Player/CameraFollow.cs
@@ -8,10 +8,29 @@ public class CameraFollow : MonoBehaviour
     public float smoothSpeed = 20f;
     public Vector3 offset;
 
+    public float minZoom = 1;
+    public float maxZoom = 20;
+    public float zoomSpeed = 4f;
+
+    private float currentZoom = 10;
+    public float yawSpeed = 100;
+    private float currentYaw = 0f;
+
+    private void Update()
+    {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+        currentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+
+    }
+
     private void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
+        Vector3 desiredPosition = target.position + offset*currentZoom;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
+
+        //transform.RotateAround(target.position, Vector3.up, currentYaw);
     }
 }
