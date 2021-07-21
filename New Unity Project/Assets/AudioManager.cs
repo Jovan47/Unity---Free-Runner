8edf00bdec002dd73905ef4478150be56e2050fe
Jovan <radulovic.jovan.ni@gmail.com>
2021-07-21 20:09:49 +0200

    Sounds Added to StartMenu

--- New Unity Project/Assets/AudioManager.cs
+++ New Unity Project/Assets/AudioManager.cs
@@ -1,26 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using System;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
     public static AudioManager instance;
-
+    public Slider volumeSlider;
     void Awake()
     {
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+           
+            Load();
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+        }
         if (instance == null)
         {
             instance = this;
         }
         else
         {
-            // Destroy(gameObject);
+             Destroy(gameObject);
             return;
         }
 
-        // DontDestroyOnLoad(gameObject);
+         DontDestroyOnLoad(gameObject);
 
         foreach (Sound s in sounds)
         { 
@@ -30,6 +40,8 @@ public class AudioManager : MonoBehaviour
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
         }
+
+        Play("BackM");
     }
 
     public void Play(string name)
@@ -46,4 +58,52 @@ public class AudioManager : MonoBehaviour
         }
     }
     //FindObjectOfType<AudioManager>().Play("SoundName");
+
+    
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        save();
+    }
+
+    public void Load()
+    {
+        if (volumeSlider != null)
+        {
+            volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        }
+        // AudioListener.volume = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    public void save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+        PlayerPrefs.Save();
+    }
+
+    public void ButtonHover()
+    {
+        PlayButtonSound("ButtonHover");
+    }
+
+    public void StartGameButton()
+    {
+        PlayButtonSound("PlayButton");
+
+    }
+
+    public void PlayButtonSound(string name)
+    {
+        Sound s=Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogError("Sound" + name +" not found");
+            return;
+        }
+        else
+        {
+            s.source.PlayOneShot(s.clip);
+        }
+    }
+
 }
