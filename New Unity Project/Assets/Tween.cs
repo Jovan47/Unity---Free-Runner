3e2b5b8a67af1c5d8d12a9feae058ae5e691af4f
Jovan <radulovic.jovan.ni@gmail.com>
2021-07-21 17:52:56 +0200

    StartMenu Finished

---
+++ New Unity Project/Assets/Tween.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class Tween : MonoBehaviour
+{
+    public float tweenTime;
+    public float timer;
+    public GameObject[] array;
+  
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 1)
+        {
+            timer = 0;
+            TweenObject();
+        }
+    }
+
+    public void TweenObject()
+    {
+        foreach (var x in array)
+        {
+            LeanTween.cancel(x);
+            transform.localScale = Vector3.one;
+            LeanTween.scale(x, Vector3.one * 1.1f, tweenTime).setEaseShake();
+        }
+    }
+
+}
